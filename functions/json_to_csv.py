e647b7cd3b5592a554920814582a31ec93eecb05
Sparsha <acsparsha2000@gmail.com>
2025-01-06 17:47:34 +0100

    Created a new file for testing

--- functions/json_to_csv.py
+++ functions/json_to_csv.py
@@ -19,7 +19,3 @@ def json_to_csv(json_file_path, csv_file_path):
             writer.writerow(v)
 
 
-if __name__ == '__main__':
-    json_file_path = r'D:\Sparsha\Fishing\routing-tool\tests\test-data\Example.json'  # Path to JSON file
-    csv_file_path = r'D:\Sparsha\Fishing\routing-tool\tests\test-dataoutput.csv'   # Path to save the CSV file
-    json_to_csv(json_file_path, csv_file_path)

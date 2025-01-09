63e895804ebe7b5f6cbaab3ad65e6bb37006234f
Shivam Suri <shivamsuri0503@gmail.com>
2025-01-09 15:06:58 +0100

    Calling backend api from frontend

---
+++ src/backend/fetch_path.py
@@ -0,0 +1,4 @@
+def fetch_paths(src, dst, height, buffer_height):
+    print("Inside fetch paths function")
+    print(src, dst, height, buffer_height)
+    return [[src, dst], [height, buffer_height]]

823fd3a919b8d25f2da29a88ec0c5535d02755f0
Doga Bahar <dogabahar51@gmail.com>
2025-01-11 22:04:19 +0100

    add threshol values on load index and condition score

--- scripts/add_allowed_height.py
+++ scripts/add_allowed_height.py
@@ -3,7 +3,11 @@ import json
 import os
 from functions.save_retrieve_graph import save_graph_sample, use_saved_graph_sample
 
+
 def update_graph(graph, height_data, bridge_data):
+    
+    condition_score_threshold = 3
+    load_index_threshold = 2
     
     for key, value in height_data.items():
         # Extract nodes
@@ -24,16 +28,20 @@ def update_graph(graph, height_data, bridge_data):
         # Check condition_score and load_index, and use assumed values if they are 0
         condition_score = value['condition_score'] if value['condition_score'] != 0 else value['assumed_condition_score']
         load_index = value['load_index'] if value['load_index'] != 0 else value['assumed_load_index']
-        print(node1, node2)
+        
         # Check and update the edge in the graph
         if graph.has_edge(node1, node2):
-            for key, attributes in graph.get_edge_data(node1, node2).items():
-                attributes['condition_score'] = condition_score
-                attributes['load_index'] = load_index
+            if condition_score > condition_score_threshold or load_index > load_index_threshold:
+                graph.remove_edge(node1, node2)
+            else:
+                graph[node1][node2][0]['condition_score'] = condition_score
+                graph[node1][node2][0]['load_index'] = load_index
         elif graph.has_edge(node2, node1):  # Check for reverse direction
-            for key, attributes in graph.get_edge_data(node2, node1).items():
-                attributes['condition_score'] = condition_score
-                attributes['load_index'] = load_index
+            if condition_score > condition_score_threshold or load_index > load_index_threshold:
+                graph.remove_edge(node2, node1)
+            else:
+                graph[node2][node1][0]['condition_score'] = condition_score
+                graph[node2][node1][0]['load_index'] = load_index
 
     BASE_PATH = os.getcwd()
     output_path = os.path.join(BASE_PATH, "routing-tool", "data", "updated_graph.graphml")
@@ -46,7 +54,6 @@ if __name__ == "__main__":
     
     # Get the path of the current script
     script_dir = os.path.dirname(__file__)
-    
     # Construct the path to the JSON file in the data folder
     height_path = os.path.join(script_dir, 'edges_and_height.json')
     bridges_path = os.path.join(script_dir, 'edges_and_bridges.json')
